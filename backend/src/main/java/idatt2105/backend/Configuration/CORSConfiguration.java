d1254459a56bc28162601f8e757129f4dff2f214
rokasb <rokasb@stud.ntnu.no>
2021-05-13 12:54:42 +0200

    .

--- backend/src/main/java/idatt2105/backend/Configuration/CORSConfiguration.java
+++ backend/src/main/java/idatt2105/backend/Configuration/CORSConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 /**
  * Configures CORS to correct localhost port
  */
