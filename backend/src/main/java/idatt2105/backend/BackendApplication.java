2a4742b8d9b11a41215f61ca78ba4fae93d4cf25
Diderik Kramer <diderikk@stud.ntnu.no>
2021-05-15 12:37:00 +0200

    fix

--- backend/src/main/java/idatt2105/backend/BackendApplication.java
+++ backend/src/main/java/idatt2105/backend/BackendApplication.java
@@ -1,14 +1,7 @@
 package idatt2105.backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import idatt2105.backend.Model.DTO.POSTSectionDTO;
-import idatt2105.backend.Service.RoomService;
-import idatt2105.backend.Service.SectionService;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -16,22 +9,4 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
-	@Autowired
-	SectionService sectionService;
-
-	@Autowired
-	RoomService roomService;
-
-	@Bean
-    public CommandLineRunner run(){
-        return args -> {
-            roomService.addSectionToRoom(new POSTSectionDTO("Board 4", "A4-112"));
-            roomService.addSectionToRoom(new POSTSectionDTO("Board 5", "A4-112"));
-            roomService.addSectionToRoom(new POSTSectionDTO("Board 3", "A4-112"));
-
-            
-        };
-    }
-
 }
