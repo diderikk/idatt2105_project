94e35ac7c56d3db18ed08d99d950df6e148aed92
rokasb <r.bliudzius@gmail.com>
2021-05-17 17:50:07 +0200

    hotfix jsonProperty isAdmin in userDTOs

--- backend/src/main/java/idatt2105/backend/Model/DTO/POSTUserDTO.java
+++ backend/src/main/java/idatt2105/backend/Model/DTO/POSTUserDTO.java
@@ -2,6 +2,8 @@ package idatt2105.backend.Model.DTO;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,5 +17,6 @@ public class POSTUserDTO {
     private String email;
     private String phoneNumber;
     private LocalDate expirationDate;
+    @JsonProperty("isAdmin")
     private boolean isAdmin;
 }
