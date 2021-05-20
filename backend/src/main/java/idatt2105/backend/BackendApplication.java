c0e39546db423a01964d67c51560930a4cd765cf
rokasb <r.bliudzius@gmail.com>
2021-05-20 15:48:47 +0200

    delete unecessery import

--- backend/src/main/java/idatt2105/backend/BackendApplication.java
+++ backend/src/main/java/idatt2105/backend/BackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import idatt2105.backend.Model.DTO.POSTSectionDTO;
 import idatt2105.backend.Model.DTO.POSTUserDTO;
