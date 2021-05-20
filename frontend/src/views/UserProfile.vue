f03ae212e13cad4fc1b8b7401b57228fdfa98230
rokasb <r.bliudzius@gmail.com>
2021-05-20 09:58:18 +0200

    added profile components

--- frontend/src/views/UserProfile.vue
+++ frontend/src/views/UserProfile.vue
@@ -1,20 +1,46 @@
 <template>
-  <div class="box">
+  <div class="card">
+    <div class="card-content">
+      <div id="block">
+        <div class="title">Information:</div>
+        <span v-if="user.isAdmin" class="tag is-dark is-medium">Admin</span>
+        <p>Name: {{ user.firstName }} {{ user.lastName }}</p>
+        <p>Email: {{ user.email }}</p>
+        <p>Phone number: {{ user.phoneNationalCode }}{{ user.phoneNumber }}</p>
+      </div>
+      <div id="block">
+        <div class="title">Statistics:</div>
+        <p>Total reservations: {{ userStats.totalReservations }}</p>
+        <p>Hours booked: {{ userStats.hoursOfReservations }}</p>
+        <span v-if="user.expirationDate !== null"
+          ><p>Expiration date: {{ user.expirationDate }}</p>
+        </span>
+        <span v-if="userStats.favouriteRoom !== null"
+          ><p>Favourite room: {{ userStats.favouriteRoom.roomCode }}</p>
+        </span>
+        <span v-if="userStats.favouriteSection !== null"
+          ><p>
+            Favourite section: {{ userStats.favouriteSection.sectionName }}
+          </p>
+        </span>
+      </div>
+    </div>
+    <footer class="card-footer">
+      <a href="#" class="card-footer-item">Edit</a>
+      <a href="#" class="card-footer-item">Delete</a>
+    </footer>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref, Ref } from "vue";
 import POSTSection from "../interfaces/Section/POSTSection.interface";
-import UserForm from "../interfaces/User/UserForm.interface";
+import User from "../interfaces/User/User.interface";
 import UserStats from "../interfaces/User/UserStats.interface";
 import { store } from "../store";
-import { UserToUserForm } from "../utils/userUtils";
 export default defineComponent({
   name: "UserProfile",
-  components: {
-      
-  },
+  components: {},
   props: {
     id: {
       required: true,
@@ -22,49 +48,53 @@ export default defineComponent({
     },
   },
   setup(props) {
-    const user: Ref<UserForm> = ref({
+    const user: Ref<User> = ref({
+      userId: -1,
       firstName: "",
       lastName: "",
       email: "",
       phoneNationalCode: "",
       phoneNumber: "",
+      isAdmin: false,
       expirationDate: "",
     });
     const userStats: Ref<UserStats> = ref({
-        hoursOfReservations: 0,
-        totalReservations: 0,
-        favouriteRoom: {
-          roomCode: "",
-          sections: [] as POSTSection[],
-        },
-        favouriteSection: {
-          sectionName: "",
-        },
+      hoursOfReservations: 0,
+      totalReservations: 0,
+      favouriteRoom: {
+        roomCode: "",
+        sections: [] as POSTSection[],
+      },
+      favouriteSection: {
+        sectionName: "",
+      },
     });
-    const isDoneLoadingUser = ref(false);
-    const isDoneLoadingStatistics = ref(false);
 
     onMounted(async () => {
       const response = await store.dispatch("getUser", props.id);
       if (response !== null) {
-        user.value = UserToUserForm(response.data);
-        isDoneLoadingUser.value = true;
+        user.value = response;
       }
 
       const statistics = await store.dispatch("getUserStatistics", props.id);
       if (statistics !== null) {
         userStats.value = statistics.data;
-        isDoneLoadingStatistics.value = true;
       }
     });
 
     return {
       user,
       userStats,
-      isDoneLoadingUser,
-      isDoneLoadingStatistics,
     };
   },
 });
 </script>
-<style scoped></style>
+<style scoped>
+span {
+  margin-bottom: 20px;
+}
+
+#block {
+  margin-bottom: 20px;
+}
+</style>
