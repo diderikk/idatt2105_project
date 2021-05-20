7a70ec4633dc81f66948a00f2b2d6317bf8f6a8d
Diderik Kramer <diderikk@stud.ntnu.no>
2021-05-20 10:03:25 +0200

    Fix get available rooms

--- backend/src/main/java/idatt2105/backend/Repository/RoomRepository.java
+++ backend/src/main/java/idatt2105/backend/Repository/RoomRepository.java
@@ -1,6 +1,5 @@
 package idatt2105.backend.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,18 +21,4 @@ public interface RoomRepository extends JpaRepository<Room, String> {
     "JOIN section ON section.section_id = reservation_section.section_id " +
     "WHERE section.room_code = ?1 AND reservation.end_time <= NOW()", nativeQuery = true)
     Optional<Long> getTotalHoursBooked(String roomCode);
-
-    @Query(value = "SELECT room.* FROM room where room.room_code NOT IN "+
-    "(SELECT room.room_code FROM room JOIN section ON (room.room_code = section.room_code) JOIN reservation_section" +
-    " ON (section.section_id = reservation_section.section_id) JOIN reservation" +
-    " ON (reservation_section.reservation_id = reservation.reservation_id AND (?1 BETWEEN reservation.start_time AND reservation.end_time OR ?2 BETWEEN reservation.start_time AND reservation.end_time" +
-    " OR (?1 <= reservation.start_time AND ?2 >= reservation.end_time))) GROUP BY room.room_code)", nativeQuery = true)
-    List<Room> getAvailableRooms(LocalDateTime startTime, LocalDateTime endTime);
-
-    @Query(value = "SELECT room.* FROM room where room.room_code NOT IN "+
-    "(SELECT room.room_code FROM room JOIN section ON (room.room_code = section.room_code) JOIN reservation_section" +
-    " ON (section.section_id = reservation_section.section_id) JOIN reservation" +
-    " ON (reservation_section.reservation_id = reservation.reservation_id AND reservation.reservation_id != ?3 AND (?1 BETWEEN reservation.start_time AND reservation.end_time OR ?2 BETWEEN reservation.start_time AND reservation.end_time" +
-    " OR (?1 <= reservation.start_time AND ?2 >= reservation.end_time))) GROUP BY room.room_code)", nativeQuery = true)
-    List<Room> getAvailableRooms(LocalDateTime startTime, LocalDateTime endTime, Long reservationId);
 }
